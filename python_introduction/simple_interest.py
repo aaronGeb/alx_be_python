d699091d3ff7c189f1307781018c4e9cc1a8ee42
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-07 08:37:51 +0300

    Add a simples interest cala.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# Calculate simple interest
+principal = 1000  # Principal amount
+rate = 5          # Rate of interest per annum
+time = 3          # Time in years
+simple_interest = (principal * rate * time) / 100
+print(f'Simple interest is:{simple_interest}')
