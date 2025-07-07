e90b06db0661cfab7448e27de098c0f519846e46
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-07 08:51:16 +0300

    Add arectangel cal

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Calculate the area of a rectangle
 
-length = 5  # Length of the rectangle
-width = 3   # Width of the rectangle
+length = 10  # Length of the rectangle
+width = 5   # Width of the rectangle
 area = length * width  # Area calculation
 print(f'The area of the rectangle is: {area}')  # Output the area
