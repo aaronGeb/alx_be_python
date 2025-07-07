0f3024e19d524e4e0831dd496e68488681393ceb
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-07 09:09:57 +0300

    Add age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Future Age Calculator
+current_age = int(input('How old are you?'))
+print('In 2050, you will be {} years old.' .format(current_age + 27))
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Convert hours to seconds
 hours = 2  # Number of hours to convert
-
 seconds = hours * 3600
 print(hours, "hour(s) is", seconds, "seconds.")
