ae3377fb456fc48ba249a1c4bd76a12747c2f38a
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-14 11:20:50 +0300

    Add control flow script

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# weather advice based on conditions
+weather = input("What is the weather like today? ")
+if weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
