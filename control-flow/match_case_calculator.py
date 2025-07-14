acf175fd6d2af5cd21f1e556786aa11432163a6b
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-14 11:51:31 +0300

    Add a basic calculator using match

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # match case calculator
-num1, num2 = map(
-    int, input(" Enter the first number: and Enter the second number:").split()
-)
-
+#num1, num2 = map(int, input(" Enter the first number: and Enter the second number:").split())
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
 operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
