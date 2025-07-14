53264f6b34bb668a6a4b1067a7ec1dc973de610c
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-14 11:48:29 +0300

    Add a basic calculator using match

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # match case calculator
-#num1, num2 = map(int,input("Enter two numbers: ").split())
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-operation = input("Enter operation (+, -, *, /): ")
+num1, num2 = map(
+    int, input(" Enter the first number: and Enter the second number:").split()
+)
+
+operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
-        print(f"{num1} + {num2} = {num1 + num2}")
+        print(f"The result is {num1 + num2}")
     case "-":
-        print(f"{num1} - {num2} = {num1 - num2}")
+        print(f"The result is {num1 - num2}")
     case "*":
-        print(f"{num1} * {num2} = {num1 * num2  }")
+        print(f"The result is {num1 * num2  }")
     case "/":
         if num2 != 0:
-            print(f"{num1} / {num2} = {num1 / num2}")
+            print(f"The result is {num1 / num2}")
         else:
             print("Error: Division by zero is not allowed.")
     case _:
         print("Invalid operation. Please enter +, -, *, or /.")
-        print("Please try again.")
