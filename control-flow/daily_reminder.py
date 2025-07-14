6a33048d5d9dc4dcf2fd752eb6d900074e3be8a0
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-14 12:57:07 +0300

    Add a daily schedule reminder script

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # personal daily reminder script
-task = input("Enter your task:")
+description = input("Enter your description:")
 priority = input("Priority (high, medium, low):")
 time_bound = input("Is it time bound (yes or no):")
 match priority:
     case "high":
-        reminder = f"Reminder: {task} is a high priority task"
+        reminder = f"Reminder: {description} is a high priority description"
     case "medium" | "low":
-        reminder = f"Note: {task} is a medium priority task"
+        reminder = f"Note: {description} is a medium priority description"
         
 if time_bound.lower() == "yes":
-    reminder += " This task requires immediate attention today!"
+    reminder += " This description requires immediate attention today!"
 elif time_bound.lower() == "no":
     reminder += ". Consider completing it when you have free time"
 print(reminder)
