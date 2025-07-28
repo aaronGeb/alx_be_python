0d6fb47961c4035618984e8070c603469810f6f0
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-07-28 13:16:49 +0300

    Add a test class to the simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -22,7 +22,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 0), None)
         self.assertEqual(self.calc.divide(-10, -2), 5)
